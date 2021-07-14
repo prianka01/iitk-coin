b5c5cfd3985619516e8a99c5b4b7fc06386db78d
Priyanka Jalan <prijalan2001@gmail.com>
2021-07-14 21:45:27 +0530

    Add endpoint to access full records

--- user/records/records.go
+++ user/records/records.go
@@ -61,8 +61,50 @@ func ViewRecords(w http.ResponseWriter, r *http.Request) {
     var transaction model.Transaction
 	var record string
     for rows.Next() {
-        rows.Scan(&transaction.Type,&transaction.Sender,&transaction.Reciever,&transaction.Amount,&transaction.Tax,&transaction.TimeStamp)
-        record=record+transaction.Type+" "+strconv.Itoa(transaction.Reciever)+ " from "+strconv.Itoa(transaction.Sender)+": Amount="+strconv.Itoa(transaction.Amount)+" with tax="+strconv.FormatFloat(transaction.Tax,'f',-1,64)+" at time-"+transaction.TimeStamp;
+        rows.Scan(&transaction.Type,&transaction.Sender,&transaction.Reciever,&transaction.Amount,&transaction.Tax,&transaction.TimeStamp,&transaction.Info)
+        record=record+transaction.Type+" "+strconv.Itoa(transaction.Reciever)+ " from "+strconv.Itoa(transaction.Sender)+": Amount="+strconv.Itoa(transaction.Amount)+" with tax="+strconv.FormatFloat(transaction.Tax,'f',-1,64)+" at time-"+transaction.TimeStamp+" extra info="+transaction.Info;
+		record+="\n";
+    }
+	if err := tx.Commit(); err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(record)
+	return
+}
+
+func FullRecords(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	tokenString := r.Header.Get("Authorization")
+	var res model.ResponseResult
+	access,err:=secretpage.HasAccess(tokenString,"gensec")
+	if err!=nil {
+		res.Error = "Please try again!"
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+	if !access {
+		res.Error = "Access to the page denied"
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+	database, _ := getdatabase.GetDatabase()
+	var ctx context.Context
+	ctx=r.Context()
+	tx, err := database.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+	 if err != nil {
+        res.Error = err.Error()
+		json.NewEncoder(w).Encode(res)
+    }
+	rows, err := database.Query("SELECT * FROM Transactions");
+    if err!=nil {
+		tx.Rollback()
+		panic(err)
+	}
+    var transaction model.Transaction
+	var record string
+    for rows.Next() {
+        rows.Scan(&transaction.Type,&transaction.Sender,&transaction.Reciever,&transaction.Amount,&transaction.Tax,&transaction.TimeStamp,&transaction.Info)
+        record=record+transaction.Type+" "+strconv.Itoa(transaction.Reciever)+ " from "+strconv.Itoa(transaction.Sender)+": Amount="+strconv.Itoa(transaction.Amount)+" with tax="+strconv.FormatFloat(transaction.Tax,'f',-1,64)+" at time-"+transaction.TimeStamp+" extra info="+transaction.Info;
 		record+="\n";
     }
 	if err := tx.Commit(); err != nil {
