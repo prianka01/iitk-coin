bffc9963a78efcbafbce196d292a1383588338d8
Priyanka Jalan <prijalan2001@gmail.com>
2021-07-14 21:42:02 +0530

    Updated with redeem request and list sore models

--- model/model.go
+++ model/model.go
@@ -22,4 +22,17 @@ type Transaction struct {
 	Amount    int     `json:"amount"`
 	Tax       float64 `json:"tax"`
 	TimeStamp string  `json:"timestamp"`
+	Info      string  `json:"info"`
+}
+type RedeemRequest struct {
+	Status string `json:"status"`
+	Item   string `json:"item"`
+	Sender int    `json:"sender"`
+	Info   string `json:"info"`
+}
+
+type StoreItems struct {
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+	Price    int    `json:"price"`
 }
