759d0d81fc4b3279894fd81bc24f23fdd5515604
Priyanka Jalan <prijalan2001@gmail.com>
2021-07-14 21:48:25 +0530

    Add new http endpoints

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"iitk-coin/user/register"
 
 	"iitk-coin/pages/coin"
+	"iitk-coin/pages/listitems"
+	"iitk-coin/pages/redeemcoins"
 	"iitk-coin/pages/secretpage"
 	"iitk-coin/user/login"
 	"iitk-coin/user/records"
@@ -27,6 +29,16 @@ func main() {
 	 if err!=nil {
 		panic(err)
 	}
+	statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS Store (Indice INTEGER, Name TEXT, Quantity INTEGER, Price INTEGER)")
+    statement.Exec()
+	 if err!=nil {
+		panic(err)
+	}
+	statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS RedeemRequests (Item TEXT, Sender INTEGER, Status TEXT, Info TEXT)")
+    statement.Exec()
+	 if err!=nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/signup", register.RegisterHandler).
 		Methods("POST","OPTIONS")
@@ -41,6 +53,14 @@ func main() {
 	r.HandleFunc("/transfercoins", coin.TransferCoins).
 		Methods("POST","OPTIONS")
 	r.HandleFunc("/records", records.ViewRecords).
+		Methods("POST","OPTIONS")
+	r.HandleFunc("/fullrecords", records.FullRecords).
+		Methods("POST","OPTIONS")
+	r.HandleFunc("/additem", listitems.AddItem).
+		Methods("POST","OPTIONS")
+	r.HandleFunc("/redeem",redeemcoins.RedeemCoins).
+		Methods("POST","OPTIONS")
+	r.HandleFunc("/acceptredeem",redeemcoins.AcceptRequest).
 		Methods("POST","OPTIONS")
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
     
